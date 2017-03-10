071cd5cc0fcd4b2145da43640da1a75522e83439
IvyXia123 <ivyHsia126.com>
2017-03-10 16:18:30 +0800

    changes for drag.js

--- drag.js
+++ drag.js
@@ -5,6 +5,7 @@ window.onload=function(){
     var a=1;
     var disX=0;
     var disY=0;
+    var num=0;
     var oDiv=document.getElementById("div1");
     oDiv.onclick=function(){
         console.log("click事件");
