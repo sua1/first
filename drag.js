dcb40b0ccd9723fd9a18891ead40cc34a42c4895
IvyXia123 <ivyHsia126.com>
2017-03-10 14:01:08 +0800

    changes for demo.html and drag.js

--- drag.js
+++ drag.js
@@ -3,6 +3,8 @@
  */
 window.onload=function(){
     var a=1;
+    var disX=0;
+    var disY=0;
     var oDiv=document.getElementById("div1");
     oDiv.onclick=function(){
         console.log("click事件");
